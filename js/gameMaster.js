f9e9f4d046ac9e73230faeff46db7afe95627c18
Raphael Vandon <r.vandon@criteo.com>
2015-01-24 10:33:40 +0100

    name actions

--- js/gameMaster.js
+++ js/gameMaster.js
@@ -13,19 +13,19 @@ function GameMaster(players)
 			var wantedY = player.gridPosition.y;
 
 			var action = player.programmedActions.pop();
-			if(action == player.controls.left && player.gridPosition.x > 0 && level[player.gridPosition.y][player.gridPosition.x-1] == '.') // left
+			if(action == LEFT && player.gridPosition.x > 0 && level[player.gridPosition.y][player.gridPosition.x-1] == '.') // left
 			{
 				wantedX--;
 			}
-			else if(action == player.controls.right && player.gridPosition.x < level[0].length-1 && level[player.gridPosition.y][player.gridPosition.x+1] == '.') // right
+			else if(action == RIGHT && player.gridPosition.x < level[0].length-1 && level[player.gridPosition.y][player.gridPosition.x+1] == '.') // right
 			{
 				wantedX++;
 			}
-			else if(action == player.controls.up && player.gridPosition.y > 0 && level[player.gridPosition.y-1][player.gridPosition.x] == '.') // up
+			else if(action == UP && player.gridPosition.y > 0 && level[player.gridPosition.y-1][player.gridPosition.x] == '.') // up
 			{
 				wantedY--;
 			}
-			else if(action == player.controls.down && player.gridPosition.y < level.length-1 && level[player.gridPosition.y+1][player.gridPosition.x] == '.') // down
+			else if(action == DOWN && player.gridPosition.y < level.length-1 && level[player.gridPosition.y+1][player.gridPosition.x] == '.') // down
 			{
 				wantedY++;
 			}
--- js/player.js
+++ js/player.js
@@ -1,6 +1,11 @@
 maxActionsToProgram = 3;
 var controlsPad = {up:12, down:13, left:14, right:15};
 
+LEFT = "left";
+RIGHT = "right";
+UP = "up";
+DOWN = "down";
+
 function Player(bitmap, position, controls, gamepadId)
 {
 	this.internalBitmap = bitmap;
@@ -40,28 +45,28 @@ function Player(bitmap, position, controls, gamepadId)
 			{
 				if(!this.prevState[this.controls.left] && isKeyPressed[this.controls.left]) // left
 				{
-					this.programmedActions.push(this.controls.left);
+					this.programmedActions.push(LEFT);
 					createjs.Sound.play(commandSetSound);
 				}
 				this.prevState[this.controls.left] = isKeyPressed[this.controls.left];
 
 				if(!this.prevState[this.controls.right] && isKeyPressed[this.controls.right]) // right
 				{
-					this.programmedActions.push(this.controls.right);
+					this.programmedActions.push(RIGHT);
 					createjs.Sound.play(commandSetSound);
 				}
 				this.prevState[this.controls.right] = isKeyPressed[this.controls.right];
 
 				if(!this.prevState[this.controls.up] && isKeyPressed[this.controls.up]) // up
 				{
-					this.programmedActions.push(this.controls.up);
+					this.programmedActions.push(UP);
 					createjs.Sound.play(commandSetSound);
 				}
 				this.prevState[this.controls.up] = isKeyPressed[this.controls.up];
 
 				if(!this.prevState[this.controls.down] && isKeyPressed[this.controls.down]) // down
 				{
-					this.programmedActions.push(this.controls.down);
+					this.programmedActions.push(DOWN);
 					createjs.Sound.play(commandSetSound);
 				}
 				this.prevState[this.controls.down] = isKeyPressed[this.controls.down];
@@ -73,25 +78,25 @@ function Player(bitmap, position, controls, gamepadId)
 			{
 				if (this.prevStatePad[controlsPad.left] == 0 && gamepads[this.gamepadId].buttons[controlsPad.left].value == 1)
 				{
-					this.programmedActions.push(this.controls.left);
+					this.programmedActions.push(LEFT);
 				}
 				this.prevStatePad[controlsPad.left] = gamepads[this.gamepadId].buttons[controlsPad.left].value;
 
 				if (this.prevStatePad[controlsPad.right] == 0 && gamepads[this.gamepadId].buttons[controlsPad.right].value == 1)
 				{
-					this.programmedActions.push(this.controls.right);
+					this.programmedActions.push(RIGHT);
 				}
 				this.prevStatePad[controlsPad.right] = gamepads[this.gamepadId].buttons[controlsPad.right].value;
 
 				if (this.prevStatePad[controlsPad.up] == 0 && gamepads[this.gamepadId].buttons[controlsPad.up].value== 1)
 				{
-					this.programmedActions.push(this.controls.up);
+					this.programmedActions.push(UP);
 				}
 				this.prevStatePad[controlsPad.up] = gamepads[this.gamepadId].buttons[controlsPad.up].value;
 
 				if (this.prevStatePad[controlsPad.down] == 0 && gamepads[this.gamepadId].buttons[controlsPad.down].value == 1)
 				{
-					this.programmedActions.push(this.controls.down);
+					this.programmedActions.push(DOWN);
 				}
 				this.prevStatePad[controlsPad.down] = gamepads[this.gamepadId].buttons[controlsPad.down].value;
 			}
